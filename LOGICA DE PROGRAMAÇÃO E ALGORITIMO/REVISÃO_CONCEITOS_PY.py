8bbcf683ba9a4747734f6f516f1bda34036c4700
Miguelbto <miguel.b.oliveira7@aluno.senai.br>
2025-05-07 08:21:17 -0300

    add
    adição de arquivos a pasta senai

---
+++ LOGICA DE PROGRAMAÇÃO E ALGORITIMO/REVISÃO_CONCEITOS_PY.py
@@ -0,0 +1,46 @@
+#exercício de FOR I IN RANGE
+"""
+acum = 0
+cont = 0
+for nota in range (1,6):
+    nota = float(input("digite sua %i: "% notas+1))
+    acum = acum + nota  #o acum serve para guardar a nota anterior
+    #cont = cont + 1
+    #print("A média da sua nota é: ", acum / cont)
+print("A média das suas notas é: ", acum)
+
+tabuada = int(input("Usúario qual valor você quer ver da tabuada? "))
+
+for i in range (0,11):
+    print(f"{tabuada} * {i} = {tabuada * i}")
+
+
+
+"""
+
+#exercício de WHILE / WHILE TRUE
+
+"""
+
+while true:
+    nome = input("Digite seu nome: ")
+    ingresso = int(input("Digite seu ingresso"))
+
+    if ingresso == 2 or ingresso == 3 or ingresso == 4:
+        print(f"Bem vindo: {nome}, bom jogo")
+
+        else:
+            print("O ingresso não bate com o cadastrado, verifique na recepção do estádio")
+    
+    if hora > 19:
+        break
+                print("você passou do horário permitido da entrada")
+ 
+"""
+
+"""
+x = 0
+while(x < 10 ):
+    print(x)
+    x = x + 1
+   """ 
---
+++ LOGICA DE PROGRAMAÇÃO E ALGORITIMO/exer_temp.py
@@ -0,0 +1,11 @@
+
+temperatura = [0,0,0,0,0,0,0,0,0,0,0,0]
+tm = 0
+tmn = 0
+t = 0
+acum = 0 
+
+for i in range (1,13, 1 ):
+    temperatura = int(input("Temperatura do mês: "))
+    acum = i + temperatura
+    
