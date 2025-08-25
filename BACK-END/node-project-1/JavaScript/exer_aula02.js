dc01b13246d2ac6ad47736307df4b3377871e4ec
Miguelbto <miguel.b.oliveira7@aluno.senai.br>
2025-08-25 15:48:00 -0300

    add exemplo e exercicios aula 2

---
+++ BACK-END/node-project-1/JavaScript/exer_aula02.js
@@ -0,0 +1,101 @@
+/*
+console.log (`O resultado da soma é: ${soma(6,8)}`)
+
+function soma(a, b) {
+    return a + b
+};
+*/
+//------------------------------------------------------------------------------------
+/*
+console.log (`O reusltado da média dessas três notas 7, 9, 10 desse aluno foi de: ${media(7,9,10)}`)
+
+function media (a,b,c) {
+    return (a + b + c) / 3
+}
+*/
+//------------------------------------------------------------------------------------
+/*
+const areaTriangulo = function (a,b) {
+    return (a * b) / 2
+}
+
+result = areaTriangulo(5,4)
+
+console.log(`O resultado da área do triangulo é de: ${result}`)
+*/
+//------------------------------------------------------------------------------------
+/*
+let a = 2
+validacao = function(a) {
+    if (a % 2 == 0 ) {
+        return console.log("Este é um número par")
+    } else {
+        return console.log("Este é um número impar")
+    }
+}
+
+result = validacao(3)
+
+*/
+
+//------------------------------------------------------------------------------------
+/*
+let maior = (a,b) => {
+
+    if (a > b) {
+        return console.log(`O número ${a} é maior`)
+    } else {
+        return console.log (`O número ${b} é maior`)
+    }
+
+}
+
+numMaior = maior(5,8)
+
+*/
+//incompleto---------------------------------------------------------------------------------
+/*
+let lista = [3, 5, 4,6,7,8]
+num = lista
+
+lista => {
+
+    if (lista.forEach(num  => console.log(num)))
+
+    return console.log([0,1,2,3,4,5] % 3 == 0 ) 
+    
+}
+
+console.log(`O numeros pares são: ${lista[2,5,56,2,2]}`)
+*/
+
+//------------------------------------------------------------------------------------
+/*
+(function (){
+
+    return console.log ("...")
+})
+()
+*/
+//------------------------------------------------------------------------------------
+
+num = 4
+
+(function(){
+    
+    return console.log (`O Quadardo de é `, num*num)
+})
+
+()
+
+
+
+
+
+
+
+
+
+
+
+
