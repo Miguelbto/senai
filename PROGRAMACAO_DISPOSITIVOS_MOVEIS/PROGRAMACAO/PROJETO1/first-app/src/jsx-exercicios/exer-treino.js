fa6ce0a436f3ea8d738a258c6cce358a7c6e32fe
miguelbto <205622748+Miguelbto@users.noreply.github.com>
2026-03-17 19:07:01 -0300

    add

--- PROGRAMACAO_DISPOSITIVOS_MOVEIS/PROGRAMACAO/PROJETO1/first-app/src/jsx-exercicios/exer-treino.js
+++ PROGRAMACAO_DISPOSITIVOS_MOVEIS/PROGRAMACAO/PROJETO1/first-app/src/jsx-exercicios/exer-treino.js
@@ -1,6 +1,11 @@
 import { View, Text, ScrollView, StyleSheet } from "react-native";
 
 export default function Lista01() {
+
+  const linguagem = "JavaScript";
+  const ano = 2025;
+
+
   return (
     <ScrollView style={styles.container}>
       <Text style={styles.titulo}>Lista 01 - SeuNome</Text>
@@ -9,10 +14,14 @@ export default function Lista01() {
         <Text style={styles.label}>Exercício 1 - View com Text</Text>
         <Text style={styles.label}>Olá</Text>
         <Text style={styles.label}>Mundo</Text>
-
-
-        
       </View>
+
+      <View style={styles.card}>
+        <Text style={styles.label}>Exercício 1 - View com Text</Text>
+        <Text style={styles.label}>{linguagem}</Text>
+        <Text style={styles.label}>{ano}</Text>
+      </View>
+
     </ScrollView>
   );
 }
